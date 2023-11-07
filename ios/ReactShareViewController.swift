dab73fe476417e28f0d772269cb041da723026f8
sajjad karimi <30430992+sajjad-karimi@users.noreply.github.com>
2023-11-07 16:18:21 +0330

    Update ReactShareViewController.swift
    remove , withExtension: "jsbundle" from line 18

--- ios/ReactShareViewController.swift
+++ ios/ReactShareViewController.swift
@@ -15,7 +15,7 @@ class ReactShareViewController: ShareViewController, RCTBridgeDelegate, ReactSha
     return RCTBundleURLProvider.sharedSettings()?
       .jsBundleURL(forBundleRoot: "index.share")
 #else
-    return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+    return Bundle.main.url(forResource: "main")
 #endif
   }
 
